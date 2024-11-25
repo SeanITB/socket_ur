785ed5582f95f88b4812216ff64e2569969f76b0
sean.costelloe.7e5 <sean.costelloe.7e5@itb.cat>
2024-11-25 15:22:14 +0100

    <<<

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,30 +1,36 @@
 package org.example
 
 import java.io.BufferedReader
-import java.io.InputStream
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
-import kotlin.concurrent.thread
 
 fun main() {
     val port = 5858
     // Obrir el servidor
     val serverSocket = ServerSocket(port)
+    println("Attempting to start socket server...")
 
     // Obrir la porta a clients
     val clientSocket = serverSocket.accept()
+    val output = PrintWriter(clientSocket.getOutputStream(), true)
 
     // Llegir el que diu el robot
     val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+    val actualPose: String = input.readLine()
+    println("This is my actual pose :$actualPose")
 
-    // Contestar
+
+    // actual pose to final pose
+    output.println("(0.222887,0.141989,0.24792,1.23286,-1.20128,-1.22264)")
+
+    //Tancar el server
+    serverSocket.close()
+}
 
 
-    val inputLine: String = input.readLine()
 
-    println("This is my actual pose :$inputLine")
-    val output = PrintWriter(clientSocket.getOutputStream(), true)
-    output.println(inputLine)
-
+fun stringToMutableList(inputLine: String): MutableList<Double> {
+    val subInput = inputLine.subSequence(2, inputLine.length -1 )
+    return subInput.split(",").map{ it.toDouble() }.toMutableList()
 }
